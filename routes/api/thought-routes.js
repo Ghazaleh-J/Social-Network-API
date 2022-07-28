adf1921aa6a9ddc81dc5dfd9514066f725334e9d
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-07-27 17:22:31 -0700

    Delete thought & reaction route done

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -30,29 +30,37 @@ router.post('/', async (req,res)=> {
 router.get('/:thoughtId', async (req,res)=> {
     try {
         const singleThought = await Thought.findOne({ _id: req.params.thoughtId })
+        .populate('reactions')
         res.status(200).json(singleThought)
     } catch(err) {
         console.log(err)
         res.status(500).json(err)
        }
 
-})
+});
 
 //TODO: ROUTE TO UPDATE A THOUGHT
 router.put('/', async (req,res)=> {
     try {
         const updatedThought = await Thought.findOneAndUpdate({ _id: req.body.thoughtId }, {$set: req.body}, {runValidators: true, new: true})
-        console.log("HEEEEEEEEEY", updatedThought)
         res.status(200).json(updatedThought)
     } catch(err) {
         console.log(err)
         res.status(500).json(err)
        }
 
-})
+});
 
 //TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
-router.delete('/:thoughtId', (req,res)=> {
+router.delete('/:thoughtId', async (req,res)=> {
+    try {
+        const deletedThought = await Thought.findOneAndRemove({ _id: req.params.thoughtId })
+        const updatedThought = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, {$pull: {thoughts: req.params.thoughtId}}, {runValidators: true, new: true})
+        res.status(200).json(deletedThought)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json(err)
+       }
 
 });
 
@@ -69,8 +77,16 @@ router.post('/:thoughtId/reactions', async (req,res)=> {
 });
 
 //TODO: ROUTE TO DELETE A REACTION ON A THOUGHT
-router.delete('/:thoughtId/reactions/:reactionId', (req,res)=> {
+router.delete('/:thoughtId/reactions/:reactionId', async (req,res)=> {
+    try {
+        const deletedReaction = await Reaction.findOneAndRemove({ _id: req.params.reactionId })
+        const updatedThought = await Thought.findOneAndUpdate({ _id: req.params.reactionId }, {$pull: {reactions: req.params.reactionId}}, {runValidators: true, new: true})
+        res.status(200).json(deletedReaction)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
 
-})
+});
 
 module.exports = router;
